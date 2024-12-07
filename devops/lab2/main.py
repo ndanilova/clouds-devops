5b12d56c0e0f6cb11f58e720a039b55c317de2a9
ndanilova <apple.for.ledi@icloud.com>
2024-12-08 01:28:46 +0300

    lab2 added

---
+++ devops/lab2/main.py
@@ -0,0 +1,10 @@
+def print_smiley():
+    print("   *****   ")
+    print(" *       * ")
+    print("*  O   O  *")
+    print("*    ^    *")
+    print(" *  ---  * ")
+    print("   *****   ")
+
+if __name__ == "__main__":
+    print_smiley()
