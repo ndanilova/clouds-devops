ebfbd7690d0b74507ae6d84025a4f56781242c98
ndanilova <apple.for.ledi@icloud.com>
2024-12-23 10:32:40 +0300

    Updated init.py

--- devops/lab3/src/__init__.py
+++ devops/lab3/src/__init__.py
@@ -7,9 +7,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"
 
 from main import get_excessive_letter  # Импорт функции из main.py
 
+
 class TestGetExcessiveLetter(unittest.TestCase):
     def test_basic_case(self):
         self.assertEqual(get_excessive_letter("abc", "abca"), "a")
+
 
 if __name__ == "__main__":
     unittest.main()
