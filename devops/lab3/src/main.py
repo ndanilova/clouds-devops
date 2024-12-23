238db18fa5a86f78e9acba24156ebc82292261d3
ndanilova <apple.for.ledi@icloud.com>
2024-12-23 10:30:11 +0300

    updated main.py

--- devops/lab3/src/main.py
+++ devops/lab3/src/main.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
     longer_sort = sorted(longer)
     shorter_sort = sorted(shorter)
@@ -7,12 +8,13 @@ def get_excessive_letter(shorter: str, longer: str) -> str:
         if shorter_sort[i] != longer_sort[i]:
             return longer_sort[i]
     return longer_sort[len(longer_sort) - 1]
-    pass
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
+
 
 if __name__ == '__main__':
     shorter, longer = read_input()
