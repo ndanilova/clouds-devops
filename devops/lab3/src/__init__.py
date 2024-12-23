665e34125e94aaf24010038689a3968f8e9c69c0
ndanilova <apple.for.ledi@icloud.com>
2024-12-23 10:35:15 +0300

    Updated init.py

--- devops/lab3/src/__init__.py
+++ devops/lab3/src/__init__.py
@@ -2,10 +2,13 @@ import sys
 import os
 import unittest
 
-# Добавляем путь к директории src в sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../src")
+    )
+)
 
-from main import get_excessive_letter  # Импорт функции из main.py
+from main import get_excessive_letter  
 
 
 class TestGetExcessiveLetter(unittest.TestCase):
